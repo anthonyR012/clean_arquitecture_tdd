70b7d185b7bfbde553868a62e2423d9c130a844f
Anthony Rubio <arubio@deltec.com.co>
2024-05-31 15:51:12 -0500

    Init commit

---
+++ lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
@@ -0,0 +1,73 @@
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tdd_test/core/error/failures.dart';
+import 'package:tdd_test/core/usecases/usecase.dart';
+
+import './bloc.dart';
+import '../../../../core/util/input_converter.dart';
+import '../../domain/usecases/get_concrete_number_trivia.dart';
+import '../../domain/usecases/get_random_number_trivia.dart';
+
+const String SERVER_FAILURE_MESSAGE = 'Server Failure';
+const String CACHE_FAILURE_MESSAGE = 'Cache Failure';
+const String INVALID_INPUT_FAILURE_MESSAGE =
+    'Invalid Input - The number must be a positive integer or zero.';
+
+class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
+  final GetConcreteNumberTrivia getConcreteNumberTrivia;
+  final GetRandomNumberTrivia getRandomNumberTrivia;
+  final InputConverter inputConverter;
+
+  NumberTriviaBloc({
+    required this.getConcreteNumberTrivia,
+    required this.getRandomNumberTrivia,
+    required this.inputConverter,
+  }) : super(Empty()) {
+    on<GetTriviaForRandomNumber>(_getTriviaForRandomNumber);
+    on<GetTriviaForConcreteNumber>(_getTriviaForConcreteNumber);
+  }
+
+  _getTriviaForRandomNumber(
+      GetTriviaForRandomNumber event, Emitter<NumberTriviaState> emit) async {
+    try {
+      emit(Loading());
+      final failureOrTrivia = await getRandomNumberTrivia(NoParams());
+      emit(failureOrTrivia.fold(
+        (failure) => Error(message: _mapFailureToMessage(failure)),
+        (trivia) => Loaded(trivia: trivia),
+      ));
+    } catch (e) {
+      emit(Empty());
+    }
+  }
+
+  _getTriviaForConcreteNumber(
+      GetTriviaForConcreteNumber event, Emitter<NumberTriviaState> emit) async {
+    try {
+      emit(Loading());
+      final inputEither =
+          inputConverter.stringToUnsignedInteger(event.numberString);
+      final integer = inputEither.foldRight(0, (r, previous) => r);
+      final failureOrTrivia =
+          await getConcreteNumberTrivia(Params(number: integer));
+      emit(failureOrTrivia.fold(
+        (failure) => Error(message: _mapFailureToMessage(failure)),
+        (trivia) => Loaded(trivia: trivia),
+      ));
+    } catch (e) {
+      emit(Empty());
+    }
+  }
+
+
+  String _mapFailureToMessage(Failure failure) {
+    switch (failure.runtimeType) {
+      case ServerFailure _:
+        return SERVER_FAILURE_MESSAGE;
+      case CacheFailure _:
+        return CACHE_FAILURE_MESSAGE;
+      default:
+        return 'Unexpected error';
+    }
+  }
+}
